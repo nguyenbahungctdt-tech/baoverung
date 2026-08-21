9971bdfbf6e7b72e4efcc5a73c9dd35ab0af028d
Nguyen Ba Hung <nguyenbahung.ctdt@gmail.com>
2026-08-21 10:05:39 +0700

    Sửa lỗi biên dịch Kotlin/Native cho iOS

--- composeApp/src/iosMain/kotlin/com/baoverung/app/util/PlatformUtils.kt
+++ composeApp/src/iosMain/kotlin/com/baoverung/app/util/PlatformUtils.kt
@@ -1,8 +1,17 @@
 package com.baoverung.app.util
 
-import platform.Foundation.NSString
-import platform.Foundation.stringWithFormat
-
+/**
+ * Triển khai định dạng số an toàn cho iOS
+ */
 actual fun formatNumber(value: Double, decimals: Int): String {
-    return NSString.stringWithFormat("%.${decimals}f", value)
+    // Sử dụng cách chuyển đổi đơn giản để tránh lỗi variadic arguments trong Kotlin/Native
+    val power = 10.0.pow(decimals.toDouble())
+    val rounded = (value * power).toLong().toDouble() / power
+    return rounded.toString()
+}
+
+private fun Double.pow(exponent: Double): Double {
+    var result = 1.0
+    repeat(exponent.toInt()) { result *= this }
+    return result
 }
