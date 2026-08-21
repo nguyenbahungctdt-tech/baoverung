e2a25fb35a948eb1da5a20e2eae1f8e789083643
Nguyen Ba Hung <nguyenbahung.ctdt@gmail.com>
2026-08-21 16:01:48 +0700

    Sửa lỗi thiếu declaration actual cho CameraManager trên iOS và Android

---
+++ composeApp/src/iosMain/kotlin/com/baoverung/app/platform/CameraManager.kt
@@ -0,0 +1,23 @@
+package com.baoverung.app.platform
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+
+actual class CameraManager {
+    @Composable
+    actual fun CameraPreview(
+        modifier: Modifier,
+        onPhotoCaptured: (String) -> Unit
+    ) {
+        Box(modifier = modifier) {
+            Text("iOS Camera Preview Placeholder", modifier = Modifier.align(Alignment.Center))
+        }
+    }
+    
+    actual fun capturePhoto() {
+        // iOS implementation will be added later
+    }
+}
