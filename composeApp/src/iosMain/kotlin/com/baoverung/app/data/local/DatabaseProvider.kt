d8edbd57e6b0fb02b6822f65d492d2e8c6b0a17c
Nguyen Ba Hung <nguyenbahung.ctdt@gmail.com>
2026-08-22 22:37:52 +0700

    Fix iOS build failure: Opt-in to ExperimentalForeignApi in DatabaseProvider

--- composeApp/src/iosMain/kotlin/com/baoverung/app/data/local/DatabaseProvider.kt
+++ composeApp/src/iosMain/kotlin/com/baoverung/app/data/local/DatabaseProvider.kt
@@ -2,10 +2,12 @@ package com.baoverung.app.data.local
 
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import kotlinx.cinterop.ExperimentalForeignApi
 import platform.Foundation.NSDocumentDirectory
 import platform.Foundation.NSFileManager
 import platform.Foundation.NSUserDomainMask
 
+@OptIn(ExperimentalForeignApi::class)
 actual class DatabaseBuilder {
     actual fun createBuilder(): RoomDatabase.Builder<AppDatabase> {
         val fileManager = NSFileManager.defaultManager
